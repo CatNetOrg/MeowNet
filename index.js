e2923b5dcd9cc671760e11c714fe3c197e55a66d
CoffeeMensch <https://github.com/CoffeeMensch>
2021-12-05 18:48:56 +0200

    Added gromatikal correcting

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
 function buttonColorFill(liColor) {
-    let SwetchButton = document.getElementsByClassName('SwetchButton');
+    let switchButton = document.getElementsByClassName('switchButton');
     liColor.style.backgroundColor = 'white';
     liColor.style.borderColor = 'white';
     liColor.style.opacity = '.8';
 
-
-    for (let i = 0; i < SwetchButton.length; i++) {
-        if (SwetchButton[i] !== liColor) {
-            SwetchButton[i].style.backgroundColor = 'transparent';
-            SwetchButton[i].style.borderColor = 'white';
-            SwetchButton[i].style.opacity= '.3';
+    for (let i = 0; i < switchButton.length; i++) {
+        if (switchButton[i] !== liColor) {
+            switchButton[i].style.backgroundColor = 'transparent';
+            switchButton[i].style.borderColor = 'white';
+            switchButton[i].style.opacity= '.3';
         }
     }
-
 }
-//.SwetchButton {
-//     margin: 50%;
-//     width: 12px;
-//     height: 12px;
-//     border-radius: 20px;
-//     border: 5px solid white;;
-//     opacity: .3;
-//     -webkit-transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
-//     transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
-//
-// }
-//
-// .SwetchButton:hover {
-//     border: 5px solid white;
-//     opacity: .7;
-// }
-
-let SwetchButton = document.getElementsByClassName('SwetchButton');
-
-function f(v) {
-    let temp = v;
-    v.style.backgroundColor = 'white';
-    v.style.borderColor = 'white';
-    v.style.opacity = '.7';
-
-    if(temp !== v) {
-        v.style.backgroundColor = 'transparent';
-    }
-}
-
 
